3fb150827e53a30b59c69ce8fb97970a66ed2a93
LacoosTe9927 <69544058+LacoosTe9927@users.noreply.github.com>
2026-09-08 01:14:09 +0200

    Change return type of initialize method in SmartFolkEntity
    Refactor the initialize method to return LivingEntity instead of SpawnGroupData.

--- src/main/java/fr/smartfolk/entity/SmartFolkEntity.java
+++ src/main/java/fr/smartfolk/entity/SmartFolkEntity.java
@@ -83,8 +83,8 @@ public class SmartFolkEntity extends PathAwareEntity {
     }
 
     @Override
-    public LivingEntity.SpawnGroupData initialize(ServerWorldAccess world, LocalDifficulty difficulty,
-                                    SpawnReason spawnReason, LivingEntity.SpawnGroupData entityData) {
+    public LivingEntity initialize(ServerWorldAccess world, LocalDifficulty difficulty,
+                                    SpawnReason spawnReason, EntityData) {
         // Attribution d'une couleur aleatoire a la naissance / au spawn.
         setColorVariant(ColorVariant.random(this.random));
         applyColorGear();
