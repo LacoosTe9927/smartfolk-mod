2c816bd3501c1860c1ee31ebaaca78553bb2fe65
LacoosTe9927 <69544058+LacoosTe9927@users.noreply.github.com>
2026-09-08 10:53:46 +0200

    Change tracking range method for SmartFolkEntity

--- src/main/java/fr/smartfolk/SmartFolkMod.java
+++ src/main/java/fr/smartfolk/SmartFolkMod.java
@@ -34,7 +34,7 @@ public class SmartFolkMod implements ModInitializer {
                 id("smart_folk"),
                 EntityType.Builder.<SmartFolkEntity>create(SmartFolkEntity::new, SpawnGroup.CREATURE)
                         .setDimensions(0.4F, 0.9F)
-                        .trackRangeBlocks(10)
+                        .maxTrackingRange(10)
                         .build("smart_folk")
         );
 
