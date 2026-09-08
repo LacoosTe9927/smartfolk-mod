9a2b78a736d14157844438a0d5f1c92fa4e66309
LacoosTe9927 <69544058+LacoosTe9927@users.noreply.github.com>
2026-09-08 10:35:26 +0200

    Add files via upload

--- src/main/java/fr/smartfolk/entity/SmartFolkEntity.java
+++ src/main/java/fr/smartfolk/entity/SmartFolkEntity.java
@@ -83,8 +83,8 @@ public class SmartFolkEntity extends PathAwareEntity {
     }
 
     @Override
-    public LivingEntity initialize(ServerWorldAccess world, LocalDifficulty difficulty,
-                                    SpawnReason spawnReason, EntityData entitydata) {
+    public LivingEntity.SpawnGroupData initialize(ServerWorldAccess world, LocalDifficulty difficulty,
+                                    SpawnReason spawnReason, LivingEntity.SpawnGroupData entityData) {
         // Attribution d'une couleur aleatoire a la naissance / au spawn.
         setColorVariant(ColorVariant.random(this.random));
         applyColorGear();
